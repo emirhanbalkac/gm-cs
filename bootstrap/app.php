2f805d1dc0dc2e99293d074b8691c02cf3b931c6
Emirhan Balkaç <emirhan.balkac@hotmail.com>
2024-12-24 21:50:49 +0300

    Remove unnecessary code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use App\Utils\ResponseUtil;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -19,14 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
                       //
                   })
                   ->withExceptions(function (Exceptions $exceptions) {
-                      $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
-                          if ($request->is('api/*')) {
-                              return false;
-                          }
-
-                          return $request->expectsJson();
-                      });
-
                       $exceptions->render(function (BaseException $e) {
                           return response()->json(
                             data:    ResponseUtil::sendError($e->getMessage()),
