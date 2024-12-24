c05935264e1fd8993ebd9a87a8592ba89901a39e
Emirhan Balkaç <emirhan.balkac@hotmail.com>
2024-12-24 20:04:36 +0300

    Add missing relation

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Invoice extends BaseModel
 {
@@ -17,5 +18,10 @@ class Invoice extends BaseModel
     public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
+    }
+
+    public function lines(): HasMany
+    {
+        return $this->hasMany(OrderLine::class, 'order_id', 'order_id');
     }
 }
